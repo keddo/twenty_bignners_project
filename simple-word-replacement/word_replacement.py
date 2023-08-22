40e0445222c073b03e333a0871ab5a7bf9736c15
keddo <adu.kedir.abdurahman@gmail.com>
2023-08-22 21:50:36 +0300

    Add function call

--- simple-word-replacement/word_replacement.py
+++ simple-word-replacement/word_replacement.py
@@ -7,3 +7,5 @@ def replace_word():
     word_to_replace = input("Enter the word to replace: ")
     word_replacement = input("Enter the word to replacement: ")
     print(strg.replace(word_to_replace, word_replacement))
+
+replace_word()
