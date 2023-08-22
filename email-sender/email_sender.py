8363746052099aa225c80a8d60d1077d8e707436
keddo <adu.kedir.abdurahman@gmail.com>
2023-08-22 21:39:21 +0300

    Import important libraries

--- email-sender/email_sender.py
+++ email-sender/email_sender.py
@@ -1,3 +1,8 @@
 # Go over to your email and setup 2 factor authentication
 # Generate app password
 # Create a function to send the mail
+
+from email.message import EmailMessage
+from password import password
+import ssl
+import smtplib
